a573293ce91ede75c780e563f8d3f3e1bc6fca0b
AimeCyuzuzo <aimecyuzuzo5@gmail.com>
2025-03-27 22:43:23 +0200

    remove firebase functions configuration

--- firebase.js
+++
@@ -1,9 +0,0 @@
-var firebaseAdmin = require("firebase-admin");
-const serviceAccount = require("../therapy-notes-sys-firebase-adminsdk-qmjqx-2817da45bf.json");
-
-firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serviceAccount)
-});
-
-
-module.exports = firebaseAdmin
